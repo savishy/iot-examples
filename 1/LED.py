d0efa4e80a80c767193dcd956109ffcf33808306
Vish <savishy.pv@gmail.com>
2018-01-07 11:44:23 +0000

    changed time

--- 1/LED.py
+++ 1/LED.py
@@ -6,7 +6,7 @@ GPIO.setup(23,GPIO.OUT)
 for x in range(0,10):
         print "LED ON"
 	GPIO.output(23,GPIO.HIGH)
-	time.sleep(100)
+	time.sleep(1)
 	print "LED OFF"
 	GPIO.output(23,GPIO.LOW)
-	time.sleep(100)
+	time.sleep(1)
